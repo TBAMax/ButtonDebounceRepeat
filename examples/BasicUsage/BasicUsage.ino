42e611db239bf47f438aa42035c6ba7136301cf2
Teemo Vaas <teemo@digiwood.ee>
2025-05-23 13:22:31 +0300

    Update BasicUsage.ino

--- examples/BasicUsage/BasicUsage.ino
+++ examples/BasicUsage/BasicUsage.ino
@@ -1,12 +1,11 @@
 #include <Arduino.h>
 #include "button.h"
 
-// Use digital pin 2 for the button
+// Use digital pin 2 for the button.Button between pin 2 and GND
 Button myButton(2);
 
 void setup() {
   Serial.begin(9600);
-  pinMode(2, INPUT_PULLUP); // Button between pin 2 and GND
 }
 
 void loop() {
@@ -14,9 +13,9 @@ void loop() {
     if (ev & B_PRESS) {
       Serial.println("Button pressed");
     }
-    if (ev & B_HOLD) {
-      Serial.println("Button held (repeat)");
-    }
+    if (ev & (B_HOLD | B_PRESS)) {
+      Serial.print("Button held (repeat)");
+    }  
     if ((ev & B_TIMEOUT) && ((ev & B_COUNT) == 1)) {
       Serial.println("Single click detected");
     }
